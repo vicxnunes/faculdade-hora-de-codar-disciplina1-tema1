4e5cd68e6bf2c9021f56c11cb3e2f5668ee602ff
VictoriaXDev <victoriaxnunes@gmail.com>
2025-03-05 21:08:01 -0300

    Atualizando código CalcularMedia.c e adicionando o executável

--- HoradeCodarIntermediario/CalcularMedia.c
+++ HoradeCodarIntermediario/CalcularMedia.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 
-int main(){}
+int main(){
+    float nota1, nota2, nota3;
+    float media;
+
+    printf ("*** Programa de Cálculo de Média ***\n");
+
+    printf("Digite a sua primeira nota: \n");
+    scanf("%f", &nota1);
+
+    printf("Digite a sua segunda nota: \n");
+    scanf("%f", &nota2);    
+
+    printf("Digite a sua terceira nota: \n");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("A média é: %.2f\n", media); // nem sempre a média vai ser um número inteiro e nem as notas. Eu coloquei 2f para mostrar 2 casas decimais mas poderia ser 1f, 3f, 4f, etc.
+
+
+    return 0;
+}
